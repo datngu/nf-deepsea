4a44e1d592cd6a21ebc067bb72c7d41ad7a1358b
Dat T Nguyen <n.dat@outlook.com>
2023-05-23 12:33:51 +0200

    metrics and losses

--- test_pipeline.sh
+++ test_pipeline.sh
@@ -8,12 +8,12 @@
 #SBATCH --mail-type=ALL
 
 
-module load BCFtools/1.10.2-GCC-8.3.0
-module load git/2.23.0-GCCcore-9.3.0-nodocs
+# module load BCFtools/1.10.2-GCC-8.3.0
+# module load git/2.23.0-GCCcore-9.3.0-nodocs
 module load Nextflow/21.03
 module load singularity/rpm
 
-git pull
+#git pull
 
 #cd /mnt/SCRATCH/ngda
 
