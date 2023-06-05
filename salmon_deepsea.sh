16fac604a0941cfae7f134c7f94af8ad4bb78a1a
Dat T Nguyen <n.dat@outlook.com>
2023-06-05 14:53:16 +0200

    add RC

--- salmon_deepsea.sh
+++ salmon_deepsea.sh
@@ -3,7 +3,6 @@
 #SBATCH --nodes=1                
 #SBATCH --job-name=DS-salmon   
 #SBATCH --mem=4G                
-#SBATCH --partition=smallmem
 #SBATCH --mail-user=nguyen.thanh.dat@nmbu.no
 #SBATCH --mail-type=ALL
 
